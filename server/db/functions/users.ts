ed5418d1861b8e090c9302b7b330200b90c432f3
Matt Spriggs <83927528+mattspriggs@users.noreply.github.com>
2023-11-21 10:33:16 +1300

    Get all profile function added

--- server/db/functions/users.ts
+++ server/db/functions/users.ts
@@ -1,2 +1,7 @@
-import {} from '../../../models/user'
+import { User, UserSnakeCase } from '../../../models/user'
 import db from '../connection'
+
+export async function getAllProfiles(): Promise<User[]> {
+  const users = await db('users').select()
+  return users
+}
