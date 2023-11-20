8f7a451834b707ff7040f899bed7b002ca6e4a35
Tayne Bilyard <124834636+TayneB@users.noreply.github.com>
2023-11-21 10:45:19 +1300

    Get all profile function added

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,10 +1,20 @@
 import express from 'express'
+import * as db from '../db/functions/users'
 
 const router = express.Router()
 
-// GET /api/v1/posts
-router.get('/', (req, res) => {
-  res.status(200).send('Hello from the users route!')
+// GET /api/v1/users
+router.get('/', async (req, res) => {
+  try {
+    const users = await db.getAllProfiles()
+    res.status(200).json(users)
+  } catch (error) {
+    res
+      .sendStatus(500)
+      .json({ message: 'Something went wrong getting all profiles' })
+  }
 })
+
+// GET /api/v1/users/:id
 
 export default router
